3b921302029bc4ba0414b1903b8518849f558993
Maciek <m.p.dutkiewicz@gmail.com>
2017-09-20 17:48:30 +0200

    init 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,33 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+const env = process.env.NODE_ENV || 'development';
+const plugins = [
+new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+      sourceMap: false
+    })
+  );
+}
+
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+            'react-hot-loader/patch',
+        './src/index.js'
+    ],
     output: {
         path: __dirname + './build',
         filename: 'app.bundle.js'
@@ -23,5 +51,14 @@ module.exports = {
                  ]
              }
          ]
-     }
+     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({sourceMap: false})
+    ]
 };
